2a90d199278c24940d633327d158e17846b4a353
Luxchar <luccharlopeau@gmail.com>
2023-05-10 13:33:33 +0200

    tree reworked

--- Structures/tree.py
+++ Structures/tree.py
@@ -1,16 +1,20 @@
-class tree:
+class Node_tree: 
+  def __init__(self, question: str, yes_node: object, no_node: object):
+    self.question = question
+    self.yes_node = yes_node
+    self.no_node = no_node
+
+  def append(self, question: str, yes_node: object, no_node: object):
+    self.yes_node.append(question,yes_node,no_node)
+    self.no_node.append(question,yes_node,no_node)
+    
+class Tree:
   def __init__(self,first_question):
-    self.first_node = node(first_question,[])
+    self.first_node = Node_tree(first_question,[])
     self.current_node = self.first_node
 
   def append_question(self,question,reponses,previous_question):
     self.first_node.append(question,reponses,previous_question)
-
-  def delete_question(self,question):
-    if self.first_node.question == question:
-      self.first_node = None
-    else:
-      self.first_node.delete(question)
 
   def get_question(self):
     return self.current_node.question
